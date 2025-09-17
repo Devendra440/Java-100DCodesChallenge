278cd24bcaf8935d9ba5f834b28dee1cf7175508
PINNAM DEVENDRA GUPTA <145124672+Devendra440@users.noreply.github.com>
2025-09-17 23:09:39 +0530

    MatrixAntiClockwiseRotation.java

---
+++ MatrixAntiClockwiseRotation.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class MatrixAntiClockwiseRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of square matrix (n): ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate90AntiClockwise(matrix);
+
+        System.out.println("Rotated Matrix (90° Anti-clockwise):");
+        printMatrix(matrix);
+
+        sc.close();
+    }
+
+    public static void rotate90AntiClockwise(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            int top = 0, bottom = n - 1;
+            while (top < bottom) {
+                int temp = matrix[top][j];
+                matrix[top][j] = matrix[bottom][j];
+                matrix[bottom][j] = temp;
+                top++;
+                bottom--;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
